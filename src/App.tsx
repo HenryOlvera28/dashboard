851974f8eccc5c15003defa1aa28dc47cfdc5e46
HenryOlvera28 <henrymoises2815@gmail.com>
2025-12-01 19:49:26 +0000

    Cambios guia 14

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import AlertUI from './components/AlertUI';
 import './App.css'
 import { Grid } from '@mui/material';
 import HeaderUI from './components/HeaderUI';
+import SelectorUI from "./components/SelectorUI";
 
 function App() {
    return (
@@ -19,7 +20,7 @@ function App() {
          </Grid>
 
          {/* Selector */}
-         <Grid size={{ xs: 12, md: 3  }}>Elemento: Selector</Grid>
+         <Grid size={{ xs: 12, md: 3  }}><SelectorUI/></Grid>
 
 
          {/* Indicadores */}
